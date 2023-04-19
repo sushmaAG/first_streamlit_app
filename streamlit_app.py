ff522b65426ecbdfdbb08e0ea63cf050fbbedc99
sushmaAG <131305645+sushmaAG@users.noreply.github.com>
2023-04-19 17:18:29 -0400

    Update streamlit_app.py
    changed the header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom''s New Healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 omeg 3 & Blueberry oatmeal')
 streamlit.text('🥗 kale,spinach and Rocket smoothie')
