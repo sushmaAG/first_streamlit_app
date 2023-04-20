cd8dccb76b3102d9f1acfe4eaa03d8bef2a4f3fd
sushmaAG <131305645+sushmaAG@users.noreply.github.com>
2023-04-20 11:14:29 -0400

    Update streamlit_app.py
    added pandas to read s3 csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 omeg 3 & Blueberry oatmeal')
@@ -6,4 +7,6 @@ streamlit.text('🥗 kale,spinach and Rocket smoothie')
 streamlit.text(' 🐔 Hard-boiled freerange eggs')
 streamlit.text(' 🥑🍞 Avacado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
                
